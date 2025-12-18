b82f1a115450b335a482cb20bcd2a1f43f3782aa
Negoita Andrei <lilbabyfan04@gmail.com>
2025-12-18 14:22:32 +0200

    added routes and now the backend connects to the db

---
+++ backend/src/routes/api.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { login, register } from '../controllers/AuthController';
+import { addVehicle, getVehicles } from '../controllers/VehicleController';
+import { createAppointment, getMyAppointments } from '../controllers/AppointmentController';
+import { getBrands, getModels, getServices } from '../controllers/DataController';
+import { protect } from '../middleware/authMiddleware';
+
+const router = Router();
+
+router.post('/auth/register', register);
+router.post('/auth/login', login);
+
+router.get('/vehicles', protect, getVehicles);
+router.post('/vehicles', protect, addVehicle);
+
+router.post('/appointments', protect, createAppointment);
+router.get('/appointments', protect, getMyAppointments);
+
+router.get('/services', getServices); 
+router.get('/brands', getBrands);     
+router.get('/models/:brandId', getModels); 
+
+export default router;
